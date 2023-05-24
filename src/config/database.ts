f5dcb1e0be0eec7743654956e00efdaa41f536e5
Alexandre <alexandremcoelho@outlook.com>
2023-05-24 07:49:07 -0300

    fix/ build

--- src/config/database.ts
+++ src/config/database.ts
@@ -5,11 +5,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "motty.db.elephantsql.com",
+  host: process.env.HOST,
   port: 5432,
-  username: "lwmrhaje",
-  password: "nGevHyOZL0RMq0igUPvy5utvHyfsMCTP",
-  database: "lwmrhaje",
+  username: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
   synchronize: false,
   logging: false,
   entities: ["src/entities/*{.ts,.js}"],
